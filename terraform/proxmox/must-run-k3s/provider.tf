7751504af6320dc03a7398731eef858ac3577073
XIIISins <xiii@xiiisins.com>
2026-05-13 23:07:29 +0200

    fix provider.tf with proper IP to proxmox

--- terraform/proxmox/must-run-k3s/provider.tf
+++ terraform/proxmox/must-run-k3s/provider.tf
@@ -1,5 +1,5 @@
 provider "proxmox" {
-  endpoint = "https://10.0.1.11:8006"
+  endpoint = "https://10.0.254.11:8006"
   api_token = var.proxmox_api_token
   insecure  = true  # self-signed cert, change later with proper cert
 }
