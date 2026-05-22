f3fe2d95634e61216ea5293ef988c27c25e92e6b
XIIISins <xiii@xiiisins.com>
2026-05-22 10:34:46 +0200

    fix(terraform): Update tf locals to use local templates and avoid migration

--- terraform/proxmox/asgard-k3s/main.tf
+++ terraform/proxmox/asgard-k3s/main.tf
@@ -1,12 +1,12 @@
 locals {
   control_planes = {
-    gondul = { node = "verd", vmid = 2001, ip = "10.0.21.11", template_node = "verd", template_id = 10002, cores = 2, memory = 4096 }
+    gondul = { node = "urd", vmid = 2001, ip = "10.0.21.11", template_node = "urd", template_id = 10006, cores = 2, memory = 4096 }
     hlokk  = { node = "verd", vmid = 2002, ip = "10.0.21.12", template_node = "verd", template_id = 10002, cores = 2, memory = 4096 }
     sigrun = { node = "skuld", vmid = 2003, ip = "10.0.21.13", template_node = "skuld", template_id = 10004, cores = 2, memory = 4096 }
   }
 
   workers = {
-    einherjar-urd   = { node = "urd", vmid = 2101, ip = "10.0.21.21", ip_vlan20 = "10.0.20.201", template_node = "verd", template_id = 10002, cores = 2, memory = 4096 }
+    einherjar-urd   = { node = "urd", vmid = 2101, ip = "10.0.21.21", ip_vlan20 = "10.0.20.201", template_node = "urd", template_id = 10006, cores = 2, memory = 4096 }
     einherjar-verd  = { node = "verd", vmid = 2102, ip = "10.0.21.22", ip_vlan20 = "10.0.20.202", template_node = "verd", template_id = 10002, cores = 2, memory = 4096 }
     einherjar-skuld = { node = "skuld", vmid = 2103, ip = "10.0.21.23", ip_vlan20 = "10.0.20.203", template_node = "skuld", template_id = 10004, cores = 2, memory = 4096 }
   }
